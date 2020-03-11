6eeaf4481493d6db537cc48bd8312aac9e3acb99
charliecondorcet <charliecondorcet@gmail.com>
2020-03-10 21:45:34 -0300

    fix(databinding): fiexd and implemented changes needed to work

--- app/src/main/java/cl/ucn/disc/dsm/charlie/randomnews/activities/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/charlie/randomnews/activities/MainActivity.java
@@ -17,13 +17,47 @@ package cl.ucn.disc.dsm.charlie.randomnews.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import cl.ucn.disc.dsm.charlie.randomnews.R;
+import cl.ucn.disc.dsm.charlie.randomnews.databinding.ActivityMainBinding;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * The Main Activity class.
+ *
+ * @author Charlie Condorcet.
+ */
 public class MainActivity extends AppCompatActivity {
 
+  /**
+   * The Logger
+   */
+  private static final Logger log = LoggerFactory.getLogger(MainActivity.class);
+
+  /**
+   * @param savedInstanceState to use.
+   */
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+
+    // Inflate the layout
+    ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+    // Assign to the main view.
+    setContentView(binding.getRoot());
+
+    // Set the toolbar
+    {
+      this.setSupportActionBar(binding.toolbar);
+    }
+
+    // The refresh
+    {
+      binding.swlRefresh.setOnRefreshListener(() -> {
+        log.debug("Refreshing ..");
+      });
+    }
+
   }
+
 }
