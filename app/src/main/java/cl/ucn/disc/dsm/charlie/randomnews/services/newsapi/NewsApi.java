3d26e1bb1dc271cfc267df81d8982a60881e0152
charliecondorcet <charliecondorcet@gmail.com>
2020-03-10 22:24:02 -0300

    feat(NewsApi): add class with API data

---
+++ app/src/main/java/cl/ucn/disc/dsm/charlie/randomnews/services/newsapi/NewsApi.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2020 Charlie Condorcet
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cl.ucn.disc.dsm.charlie.randomnews.services.newsapi;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
+
+/**
+ * Interface de acceso a los endpoints the NewsApi.org.
+ *
+ * @author Charlie Condorcet.
+ */
+public interface NewsApi {
+
+  /**
+   * The URL.
+   */
+  String BASE_URL = "https://newsapi.org/v2/";
+
+  /**
+   * The API Key.
+   */
+  String API_KEY = "88cbd58935e148a8b0d899cf25845ca6";
+
+  /**
+   * https://newsapi.org/docs/endpoints/top-headlines
+   *
+   * @param category to use as filter.
+   * @param pageSize the number of results to get.
+   * @return the call of {@link NewsApiResult}.
+   */
+  @Headers({"X-Api-Key: " + API_KEY})
+  @GET("top-headlines")
+  Call<NewsApiResult> getTopHeadlines(@Query("category") final String category,
+      @Query("pageSize") final int pageSize);
+
+  /**
+   * https://newsapi.org/docs/endpoints/everything.
+   *
+   * @return the call of {@link NewsApiResult}.
+   */
+  @Headers({"X-Api-Key: " + API_KEY})
+  // TODO: Change the list of sources.
+  @GET("everything?sources=ars-technica,wired,hacker-news,recode")
+  Call<NewsApiResult> getEverything(@Query("pageSize") final int pageSize);
+
+}
+
+
